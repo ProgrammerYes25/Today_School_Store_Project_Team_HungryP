2df77b1e21a9579cc763b0c738f7bb6ae6b865f6
yscho010612@gmail.com <yscho010612@gmail.com>
2022-07-29 15:09:01 +0900

    Initial commit

---
+++ app/src/main/java/today_school_store_project/team_hungryp/MainActivity.java
@@ -0,0 +1,14 @@
+package today_school_store_project.team_hungryp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
