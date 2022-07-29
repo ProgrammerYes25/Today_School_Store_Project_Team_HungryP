3e5bca00fd81c32853791f657b935ebf5aee465e
yscho010612@gmail.com <yscho010612@gmail.com>
2022-07-29 15:33:40 +0900

    test2

---
+++ app/src/main/java/today_school_store_project/team_hungryp/PricePageActivity.java
@@ -0,0 +1,14 @@
+package today_school_store_project.team_hungryp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PricePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_price_page);
+    }
+}
