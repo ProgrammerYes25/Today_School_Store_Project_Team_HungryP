41cadd3d2ee5b84746bfcec0421a6894c3c5d5ae
yscho010612@gmail.com <yscho010612@gmail.com>
2022-08-01 15:47:57 +0900

    이미지 추가 - textlien - 메뉴 이름 앞에 붙는 라인 - newrectangle - 신재품 아래 까리는 분홍 네모
    activitv_price_page 수정1

---
+++ app/src/main/java/today_school_store_project/team_hungryp/ui/slideshow/SlideshowFragment.java
@@ -0,0 +1,35 @@
+package today_school_store_project.team_hungryp.ui.slideshow;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import today_school_store_project.team_hungryp.databinding.FragmentSlideshowBinding;
+
+public class SlideshowFragment extends Fragment {
+
+private FragmentSlideshowBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        SlideshowViewModel slideshowViewModel =
+                new ViewModelProvider(this).get(SlideshowViewModel.class);
+
+    binding = FragmentSlideshowBinding.inflate(inflater, container, false);
+    View root = binding.getRoot();
+
+        final TextView textView = binding.textSlideshow;
+        slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+@Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
