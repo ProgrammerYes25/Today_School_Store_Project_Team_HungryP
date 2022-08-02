dd256a3d1c0200cca993d9d0aa280da91c5119c8
이 예진 <s2108@e-mirim.hs.kr>
2022-08-03 06:11:56 +0900

    activity_new_page, activity_sale_page 틀 완성

---
+++ app/src/main/java/today_school_store_project/team_hungryp/SalePageActivity.java
@@ -0,0 +1,15 @@
+package today_school_store_project.team_hungryp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SalePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sale_page);
+        setTitle("할인 상품 페이지");
+    }
+}
