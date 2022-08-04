911a4ccacb7745f4bc1d4ecb7905cfd6dcffbdb9
yscho010612@gmail.com <yscho010612@gmail.com>
2022-08-04 22:21:31 +0900

    dbAddDataPageActivity.java 데이터베이스 테이블 추가

--- app/src/main/java/today_school_store_project/team_hungryp/dbAddDataPageActivity.java
+++ app/src/main/java/today_school_store_project/team_hungryp/dbAddDataPageActivity.java
@@ -2,6 +2,9 @@ package today_school_store_project.team_hungryp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -19,5 +22,24 @@ public class dbAddDataPageActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1,itmes);
         tableList.setAdapter(adapter);
+    }
+    public class myDBHelper extends SQLiteOpenHelper{
+
+        public myDBHelper(Context context){
+            super(context, "todaySchoolStoreDB", null, 1);
+        }
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE productTable (proName CHAR(30) PRIMARY KEY NOT NULL, proPrice INTEGER NOT NULL, category CHAR(20) NOT NULL,  CHAR(20) NOT NULL, , proImage BLOB, addDay UNIXTIME);");
+            db.execSQL("CREATE TABLE popularTable (rank INTEGER PRIMARY KEY NOT NULL, popName CHAR(30) NOT NULL, popImage BLOB);");
+            db.execSQL("CREATE TABLE newTable (newName CHAR(30) PRIMARY KEY NOT NULL, newImage BLOB);");
+            db.execSQL("CREATE TABLE sealTable (sealName CHAR(30) NOT NULL,Price INTEGER NOT NULL, sealPrice INTEGER, sealImage BLOB);");
+            db.execSQL("CREATE TABLE honeyTable (mixName CHAR(30) NOT NULL, honeyName1 CHAR(30) NOT NULL, honeyNamew CHAR(30) NOT NULL, honeyImage1 BLOB, honeyImage2 BLOB);");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+
+        }
     }
 }
