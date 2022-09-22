41e76f0bbe91e47ad196732f9ac344f7e80af37d
yejin4117 <s2108@e-mirim.hs.kr>
2022-09-22 09:17:00 +0900

    MainActivity 사진 누르면 인기상품,신상품,가격,할인 페이지로 넘어가기

--- app/src/main/java/today_school_store_project/team_hungryp/MainActivity.java
+++ app/src/main/java/today_school_store_project/team_hungryp/MainActivity.java
@@ -8,31 +8,45 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-ImageView imgvPrice;
+ImageView imgvPop;
 ImageView imgvNew;
+ImageView imgvPrice;
+ImageView imgvSale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("홈");
-        imgvPrice = findViewById(R.id.imgv_price);
+        imgvPop = findViewById(R.id.imgv_pop);
         imgvNew = findViewById(R.id.imgv_new);
-        imgvPrice.setOnClickListener(imgvListener);
+        imgvPrice = findViewById(R.id.imgv_price);
+        imgvSale = findViewById(R.id.imgv_sale);
+        imgvPop.setOnClickListener(imgvListener);
         imgvNew.setOnClickListener(imgvListener);
+        imgvPrice.setOnClickListener(imgvListener);
+        imgvSale.setOnClickListener(imgvListener);
 
     }
     View.OnClickListener imgvListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             switch (view.getId()) {
-                case R.id.imgv_price:
-                    Intent intent1 = new Intent(getApplicationContext(), PricePageActivity.class);
+                case R.id.imgv_pop:
+                    Intent intent1 = new Intent(getApplicationContext(), PopularPageActivity.class);
                     startActivity(intent1);
                     break;
                 case R.id.imgv_new:
                     Intent intent2 = new Intent(getApplicationContext(), NewPageActivity.class);
                     startActivity(intent2);
+                    break;
+                case R.id.imgv_price:
+                    Intent intent3 = new Intent(getApplicationContext(), PricePageActivity.class);
+                    startActivity(intent3);
+                    break;
+                case R.id.imgv_sale:
+                    Intent intent4 = new Intent(getApplicationContext(), SalePageActivity.class);
+                    startActivity(intent4);
                     break;
             }
 
