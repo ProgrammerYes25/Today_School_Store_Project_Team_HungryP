a1212e7c345a928e2a28389a2cabe9ba64426a79
jihye1012 <yjh0510122@gmail.com>
2022-09-27 19:19:55 +0900

    홈에 메뉴 버튼 추가 ( 메뉴로 화면 전환 )

--- app/src/main/java/today_school_store_project/team_hungryp/MainActivity.java
+++ app/src/main/java/today_school_store_project/team_hungryp/MainActivity.java
@@ -1,9 +1,13 @@
 package today_school_store_project.team_hungryp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -52,4 +56,39 @@ ImageView imgvSale;
 
         }
     };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_option,menu);
+        return true;//super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu1:
+                Intent intenthome = new Intent(this,MainActivity.class);
+                startActivity(intenthome);
+                break;
+            case R.id.menu2:
+                Intent intentprice = new Intent(this,  PricePageActivity.class);
+                startActivity(intentprice);
+                break;
+            case R.id.menu3:
+                Intent intentpopular = new Intent(this,PopularPageActivity.class);
+                startActivity(intentpopular);
+                break;
+            case R.id.menu4:
+                Intent intentnew = new Intent(this, NewPageActivity.class);
+                startActivity(intentnew);
+                break;
+            case R.id.menu5:
+                Intent intentsale = new Intent(this, SalePageActivity.class);
+                startActivity(intentsale);
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
