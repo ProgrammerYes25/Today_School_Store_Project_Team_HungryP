8aed92b82893ac3aee3970c8ca9ee50acd9e12a2
yscho010612@gmail.com <yscho010612@gmail.com>
2022-11-03 20:06:03 +0900

    인기상품페이지 완성

--- app/src/main/java/today_school_store_project/team_hungryp/PopularActivity.java
+++ app/src/main/java/today_school_store_project/team_hungryp/PopularActivity.java
@@ -4,18 +4,63 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class PopularActivity extends AppCompatActivity {
+    DatabaesHelper databaesHelper;
+    SQLiteDatabase sqlDB;
+    TextView top1Text, top2Text, top3Text, top4Text, top5Text, top6Text
+    , top7Text, top8Text, top9Text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_popular);
         setTitle("인기 상품 페이지");
+
+        top1Text = findViewById(R.id.top_1_text);
+        top2Text = findViewById(R.id.top_2_text);
+        top3Text = findViewById(R.id.top_3_text);
+        top4Text = findViewById(R.id.top_4_text);
+        top5Text = findViewById(R.id.top_5_text);
+        top6Text = findViewById(R.id.top_6_text);
+        top7Text = findViewById(R.id.top_7_text);
+        top8Text = findViewById(R.id.top_8_text);
+        top9Text = findViewById(R.id.top_9_text);
+
+        databaesHelper = new DatabaesHelper(this);
+        sqlDB = databaesHelper.getReadableDatabase();
+        Cursor cursor;
+        cursor = sqlDB.rawQuery("SELECT * FROM prTable WHERE pr_popular = 1;",null);
+        setPopularList(top1Text, cursor);
+        cursor = sqlDB.rawQuery("SELECT * FROM prTable WHERE pr_popular = 2;",null);
+        setPopularList(top2Text, cursor);
+        cursor = sqlDB.rawQuery("SELECT * FROM prTable WHERE pr_popular = 3;",null);
+        setPopularList(top3Text, cursor);
+        cursor = sqlDB.rawQuery("SELECT * FROM prTable WHERE pr_popular = 4;",null);
+        setPopularList(top4Text, cursor);
+        cursor = sqlDB.rawQuery("SELECT * FROM prTable WHERE pr_popular = 5;",null);
+        setPopularList(top5Text, cursor);
+        cursor = sqlDB.rawQuery("SELECT * FROM prTable WHERE pr_popular = 6;",null);
+        setPopularList(top6Text, cursor);
+        cursor = sqlDB.rawQuery("SELECT * FROM prTable WHERE pr_popular = 7;",null);
+        setPopularList(top7Text, cursor);
+        cursor = sqlDB.rawQuery("SELECT * FROM prTable WHERE pr_popular = 8;",null);
+        setPopularList(top8Text, cursor);
+        cursor = sqlDB.rawQuery("SELECT * FROM prTable WHERE pr_popular = 9;",null);
+        setPopularList(top9Text, cursor);
+
+    }
+    public void setPopularList(TextView textView, Cursor cursor){
+        while(cursor.moveToNext()){
+            textView.setText(cursor.getString(1));
+        }
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
